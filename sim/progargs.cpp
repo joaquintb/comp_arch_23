ce93877f5e0d7f6663c249e233e28515f6372af0
PaulKokhanov1 <69466838+PaulKokhanov1@users.noreply.github.com>
2023-11-06 17:39:49 +0100

    Added Input handling, may need to look into why on my end std::span and std::to_integer are not being recognize as valid functions, maybe due to my compiler

--- sim/progargs.cpp
+++ sim/progargs.cpp
@@ -1,3 +1,35 @@
 //
 // Created by jtorresb on 5/11/23.
 //
+
+#include "progargs.hpp"
+
+
+void InputTest(int argc, char **argv){
+    //handling alternative to direct use of char **argv
+    std::span const args_view{argv, static_cast<std::size_t>(argc)};
+    std::vector<std::string> const arguments{args_view.begin() + 1, args_view.end()};
+
+    // ------------------------ READING INPUT FILE & ERROR HANDLING------------------------
+
+    if(argc - 1 != 3){
+        std::cerr << "Error: Invalid number of arguments: " << std::to_string(argc-1) << std::endl;
+        std::exit(-1);
+    }
+    if(isdigit(std::to_integer((arguments[0]))) == false){
+        std::cerr << "Error: time steps must be numeric." << std::endl;
+        std::exit(-1);
+    }
+    if(std::to_integer(arguments[0]) < 0){
+        std::cerr << "Error: Invalid number of time steps." << std::endl;
+        std::exit(-2);
+    }
+    if (!arguments[1].is_open()) {
+        std::cerr << "Error: Cannot open " <<arguments[1] <<  " for reading" << std::endl;
+        std::exit(-3);
+    }
+    if (!arguments[2].is_open()) {
+        std::cerr << "Error: Cannot open " <<arguments[2] <<  " for writing" << std::endl;
+        std::exit(-4);
+    }
+}
--- sim/progargs.hpp
+++ sim/progargs.hpp
@@ -5,4 +5,16 @@
 #ifndef COMP_ARCH_23_PROGARGS_HPP
 #define COMP_ARCH_23_PROGARGS_HPP
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <memory>
+#include <algorithm>
+#include <numbers>
+#include <span>
+
+
+void InputTest(int argc, char **argv);
+
 #endif //COMP_ARCH_23_PROGARGS_HPP
