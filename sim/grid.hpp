fb8c78568fce7c3f71702309ac8c8307fa8ce567
Joaquin Torres <joaquintobrw@gmail.com>
2023-11-08 16:39:05 +0100

    Updated header file to contain new function to compare grid with trace

--- sim/grid.hpp
+++ sim/grid.hpp
@@ -11,6 +11,7 @@ class Grid {
         Grid(int size_x, int size_y, int size_z);
         void populate(Simulation& sim, std::ifstream& inputFile);
         void display_grid();
+        bool cmp_trace(std::ifstream& trace);
     
     private:
         int size_x, size_y, size_z, n_particles, size;
