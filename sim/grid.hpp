311db82c26b7a22713042b531a9bde3d1dcf3a15
joaquintb <138466740+joaquintb@users.noreply.github.com>
2023-11-16 18:24:13 +0100

    Update grid.hpp

--- sim/grid.hpp
+++ sim/grid.hpp
@@ -25,6 +25,33 @@ class Grid {
         void repos(Simulation &sim);
         void set_to_trace(std::ifstream& trace);
         void init_acc();
+
+        template <typename T>
+          requires(std::is_integral_v<T> or std::is_floating_point_v<T>)
+        T read_binary_value(std::istream & is) {
+          T value{};
+          is.read(as_writable_buffer(value), sizeof(value));
+          return value;
+        }
+
+        template <typename T>
+          requires(std::is_integral_v<T> or std::is_floating_point_v<T>)
+        void write_binary_value(T value, std::ostream & os) {
+          os.write(as_buffer(value), sizeof(value));
+        }
+
+        template <typename T>
+          requires(std::is_integral_v<T> or std::is_floating_point_v<T>)
+        char const * as_buffer(T const & value) {
+          // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+          return reinterpret_cast<char const *>(&value);
+        }
+
+        template <typename T>
+        char * as_writable_buffer(T & value) {
+          // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+          return reinterpret_cast<char *>(&value);
+        }
     
     private:
         int size_x, size_y, size_z, n_particles, size;
