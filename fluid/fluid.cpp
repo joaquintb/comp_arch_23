4ec48ec907b644bf47d0066e0146b3ade092d856
jyanespu <132780169+jyanespu@users.noreply.github.com>
2023-11-18 11:26:53 +0100

    Update fluid.cpp
    n_blocks as attribute of sim and read/write using templates

--- fluid/fluid.cpp
+++ fluid/fluid.cpp
@@ -12,31 +12,22 @@ int main(int argc, char ** argv) {
   std::ofstream outputFile(arguments[2], std::ios::binary | std::ios::trunc);
   check_files(inputFile, arguments[1], outputFile, arguments[2]);
 
-  float ppm;
-  int num_p;
-
-  inputFile.read(reinterpret_cast<char *>(&ppm), sizeof(float));
-  inputFile.read(reinterpret_cast<char *>(&num_p), sizeof(int));
-
+  auto ppm = read_binary_value<float>(inputFile);
+  auto num_p = read_binary_value<int>(inputFile);
+  
   // Create simulation
   Simulation sim = Simulation(ppm, num_p);
 
   sim.check_positive_particles();  // Check number of particles > 0
-
-  int const nx       = floor((sim.b_max[0] - sim.b_min[0]) / sim.get_sm_len());
-  int const ny       = floor((sim.b_max[1] - sim.b_min[1]) / sim.get_sm_len());
-  int const nz       = floor((sim.b_max[2] - sim.b_min[2]) / sim.get_sm_len());
-  int const n_blocks = nx * ny * nz;
-
-  sim.print_sim_values(n_blocks);  // Print initial attributes of simulation (successful input)
+  sim.print_sim_values();  // Print initial attributes of simulation (successful input)
 
   // Create grid
-  Grid grid = Grid(nx, ny, nz);
+  Grid grid = Grid(sim.n_x, sim.n_y, sim.n_z);
   grid.populate(sim, inputFile);
   grid.set_neighbors();
 
   inputFile.close();
-
+  
   for (int time_step = 0; time_step < n_steps; ++time_step) {
     if (time_step > 0) {
       grid.repos(sim);
@@ -51,11 +42,11 @@ int main(int argc, char ** argv) {
     grid.part_box_collisions(sim);
   }
 
-  outputFile.write(reinterpret_cast<char const *>(&ppm), sizeof(float));
-  outputFile.write(reinterpret_cast<char const *>(&num_p), sizeof(int));
+  write_binary_value(static_cast<float>(ppm), outputFile);
+  write_binary_value(static_cast<int>(num_p), outputFile);
   grid.gen_output(outputFile);
 
   outputFile.close();
-
+  
   return 0;
 }
