bebe69512f3aaaf02b27847812a88f151a3a735f
Joaquin Torres <joaquintobrw@gmail.com>
2023-11-18 12:10:55 +0100

    grid cleaning up

--- sim/grid.cpp
+++ sim/grid.cpp
@@ -1,40 +1,36 @@
 #include "grid.hpp"
 
-Grid::Grid(int size_x, int size_y, int size_z) {
-  this->size_x      = size_x;
-  this->size_y      = size_y;
-  this->size_z      = size_z;
-  this->size        = size_x * size_y * size_z;
-  this->n_particles = 0;
-  this->blocks      = std::vector<Block>();
-  for (int i = 0; i < this->size; ++i) { this->blocks.push_back(Block(i, size_x, size_y, size_z)); }
-};
+Grid::Grid(int size_x, int size_y, int size_z)
+  : size_x(size_x), size_y(size_y), size_z(size_z), n_particles(0), size(size_x * size_y * size_z) {
+  this->blocks = std::vector<Block>();
+  for (int i = 0; i < this->size; ++i) { this->blocks.emplace_back(i, size_x, size_y, size_z); }
+}
 
 void Grid::populate(Simulation & sim, std::ifstream & inputFile) {
-  int num_p = sim.get_num_p();
+  int const num_p = sim.get_num_p();
   for (int i = 0; i < num_p; i++) {
     Particle cur_particle = Particle(inputFile, i);
-    int grid_index        = cur_particle.compute_grid_index(sim);
+    int const grid_index  = cur_particle.compute_grid_index(sim);
     this->blocks[grid_index].particles.push_back(cur_particle);
   }
-};
+}
 
 void Grid::set_neighbors() {
   for (int block_id = 0; block_id < this->size; ++block_id) {
     Block & curr_block = this->blocks[block_id];
     // Iterate through neighbor blocks, taking care of edge cases using min and max
     // [!] We consider that a block is neighbor of itself (useful in computations)
-    int x_start = std::max(curr_block.index_i - 1, 0);
-    int x_end   = std::min(curr_block.index_i + 1, this->size_x - 1);
-    int y_start = std::max(curr_block.index_j - 1, 0);
-    int y_end   = std::min(curr_block.index_j + 1, this->size_y - 1);
-    int z_start = std::max(curr_block.index_k - 1, 0);
-    int z_end   = std::min(curr_block.index_k + 1, this->size_z - 1);
+    int const x_start = std::max(curr_block.index_i - 1, 0);
+    int const x_end   = std::min(curr_block.index_i + 1, this->size_x - 1);
+    int const y_start = std::max(curr_block.index_j - 1, 0);
+    int const y_end   = std::min(curr_block.index_j + 1, this->size_y - 1);
+    int const z_start = std::max(curr_block.index_k - 1, 0);
+    int const z_end   = std::min(curr_block.index_k + 1, this->size_z - 1);
     for (int neig_id_x = x_start; neig_id_x <= x_end; ++neig_id_x) {
       for (int neig_id_y = y_start; neig_id_y <= y_end; ++neig_id_y) {
         for (int neig_id_z = z_start; neig_id_z <= z_end; ++neig_id_z) {
           // Compute global index of neighbor block
-          int neig_id = neig_id_x + (this->size_x) * (neig_id_y + (this->size_y) * neig_id_z);
+          int const neig_id = neig_id_x + (this->size_x) * (neig_id_y + (this->size_y) * neig_id_z);
           // Get the neighbor block
           Block & neigh_block = this->blocks[neig_id];
           // Store pointers to neighbors in the current block
@@ -45,6 +41,7 @@ void Grid::set_neighbors() {
   }
 }
 
+/*
 bool Grid::cmp_trace(std::ifstream & trace) {
   // Read the total number of blocks from the binary file
   int32_t totalBlocks;
@@ -284,30 +281,7 @@ void Grid::set_to_trace(std::ifstream & trace) {
   std::cout << "Fixed grid!" << std::endl;
 }
 
-/*
-void Grid::increase_all_dens(Simulation & sim) {
-  std::set<std::pair<int, int>> proc_pairs;
-  double const hSquared = std::pow(sim.get_sm_len(), 2);
-  for (auto & block : this->blocks) {        // For each block in the grid
-    for (auto & part_i : block.particles) {  // Iterate over particles within the current block
-      for (auto & neigh_ptr : block.neighbours) {     // Iterate over neighbor block pointer
-        for (auto & part_j : neigh_ptr->particles) {  // Iterate neigh particles
-          int id_i = part_i.pid;
-          int id_j = part_j.pid;
-          if (id_i != id_j) {
-            // (i,j) equivalent to (j,i)
-            std::pair<int, int> particle_pair(std::min(id_i, id_j), std::max(id_i, id_j));
-            // If not a processed pair
-            if (proc_pairs.find(particle_pair) == proc_pairs.end()) {
-              part_i.inc_part_dens(part_j, hSquared);
-              proc_pairs.insert(particle_pair);  // Pair processed, keep track
-            }
-          }
-        }
-      }
-    }
-  }
-} */
+*/
 
 void Grid::increase_all_dens(Simulation & sim) {
   double const hSquared = std::pow(sim.get_sm_len(), 2);
@@ -315,11 +289,9 @@ void Grid::increase_all_dens(Simulation & sim) {
     for (auto & part_i : block.particles) {  // Iterate over particles within the current block
       for (auto & neigh_ptr : block.neighbours) {     // Iterate over neighbor block pointer
         for (auto & part_j : neigh_ptr->particles) {  // Iterate neigh particles
-          int id_i = part_i.pid;
-          int id_j = part_j.pid;
-          if (id_j > id_i) {
-            part_i.inc_part_dens(part_j, hSquared);
-          }
+          long const id_i = part_i.pid;
+          long const id_j = part_j.pid;
+          if (id_j > id_i) { part_i.inc_part_dens(part_j, hSquared); }
         }
       }
     }
@@ -346,14 +318,14 @@ void Grid::increase_all_accs(Simulation & sim) {
     for (auto & part_i : block.particles) {
       for (auto & neigh_ptr : block.neighbours) {
         for (auto & part_j : neigh_ptr->particles) {
-          int id_i = part_i.pid;
-          int id_j = part_j.pid;
+          long const id_i = part_i.pid;
+          long const id_j = part_j.pid;
           if (id_j > id_i) {
-            double distanceSquared = std::pow(part_i.posX - part_j.posX, 2) +
-                                     std::pow(part_i.posY - part_j.posY, 2) +
-                                     std::pow(part_i.posZ - part_j.posZ, 2);
+            double const distanceSquared = std::pow(part_i.posX - part_j.posX, 2) +
+                                           std::pow(part_i.posY - part_j.posY, 2) +
+                                           std::pow(part_i.posZ - part_j.posZ, 2);
             if (distanceSquared < sim.sm_len_sq) {
-                part_i.inc_part_acc(part_j, sim, distanceSquared);
+              part_i.inc_part_acc(part_j, sim, distanceSquared);
             }
           }
         }
@@ -383,6 +355,7 @@ void Grid::part_collisions(Simulation & sim) {
 }
 
 void Grid::motion(Simulation & sim) {
+  double const half = 0.5;
   // For each block in the grid
   for (auto & block : this->blocks) {
     // For each particle in that block
@@ -394,9 +367,9 @@ void Grid::motion(Simulation & sim) {
       particle.posZ =
           particle.posZ + particle.hvZ * sim.delta_t + particle.accZ * sim.delta_t * sim.delta_t;
 
-      particle.velX = particle.hvX + 0.5 * particle.accX * sim.delta_t;
-      particle.velY = particle.hvY + 0.5 * particle.accY * sim.delta_t;
-      particle.velZ = particle.hvZ + 0.5 * particle.accZ * sim.delta_t;
+      particle.velX = particle.hvX + half * particle.accX * sim.delta_t;
+      particle.velY = particle.hvY + half * particle.accY * sim.delta_t;
+      particle.velZ = particle.hvZ + half * particle.accZ * sim.delta_t;
 
       particle.hvX = particle.hvX + particle.accX * sim.delta_t;
       particle.hvY = particle.hvY + particle.accY * sim.delta_t;
@@ -435,26 +408,25 @@ void Grid::repos(Simulation & sim) {
   for (auto & block : this->blocks) { block.particles.clear(); }
 
   for (auto & par : all_part) {
-    int grid_index = par.compute_grid_index(sim);
+    int const grid_index = par.compute_grid_index(sim);
     this->blocks[grid_index].particles.push_back(par);
   }
 }
 
 void Grid::init_acc() {
+  double const grav = -9.8;
   for (auto & block : this->blocks) {
     for (auto & part : block.particles) {
       part.density = 0.0;
       part.accX    = 0.0;
-      part.accY    = -9.8;
+      part.accY    = grav;
       part.accZ    = 0.0;
     }
   }
 }
 
-void Grid::gen_output(std::ofstream& out) {
-  for (auto &block: this->blocks) {
-    for (auto &particle: block.particles) {
-      particle.write_particle_output(out);
-    }
+void Grid::gen_output(std::ofstream & out) {
+  for (auto & block : this->blocks) {
+    for (auto & particle : block.particles) { particle.write_particle_output(out); }
   }
 }
