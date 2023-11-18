17a9617f77fe02e3398d7a567a0d27fe44b433d4
jyanespu <132780169+jyanespu@users.noreply.github.com>
2023-11-18 11:20:23 +0100

    Update block.hpp
    Template outside class

--- sim/block.hpp
+++ sim/block.hpp
@@ -6,6 +6,33 @@
 
 #include "simulation.hpp"
 
+template <typename T>
+  requires(std::is_integral_v<T> or std::is_floating_point_v<T>)
+char const * as_buffer(T const & value) {
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+  return reinterpret_cast<char const *>(&value);
+}
+
+template <typename T>
+char * as_writable_buffer(T & value) {
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+  return reinterpret_cast<char *>(&value);
+}
+
+template <typename T>
+  requires(std::is_integral_v<T> or std::is_floating_point_v<T>)
+T read_binary_value(std::istream & is) {
+  T value{};
+  is.read(as_writable_buffer(value), sizeof(value));
+  return value;
+}
+
+template <typename T>
+  requires(std::is_integral_v<T> or std::is_floating_point_v<T>)
+void write_binary_value(T value, std::ostream & os) {
+  os.write(as_buffer(value), sizeof(value));
+}
+
 struct Particle{
     long pid;
     double posX, posY, posZ;
@@ -25,33 +52,6 @@ struct Particle{
     void inc_part_dens (Particle & part_j, double const hSquared);
 
     void inc_part_acc (Particle &part_j, Simulation & sim, double const distanceSquared);
-
-    template <typename T>
-    requires(std::is_integral_v<T> or std::is_floating_point_v<T>)
-    T read_binary_value(std::istream & is) {
-        T value{};
-        is.read(as_writable_buffer(value), sizeof(value));
-        return value;
-    }
-
-    template <typename T>
-    requires(std::is_integral_v<T> or std::is_floating_point_v<T>)
-    void write_binary_value(T value, std::ostream & os) {
-        os.write(as_buffer(value), sizeof(value));
-    }
-
-    template <typename T>
-    requires(std::is_integral_v<T> or std::is_floating_point_v<T>)
-    char const * as_buffer(T const & value) {
-        // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-        return reinterpret_cast<char const *>(&value);
-    }
-
-    template <typename T>
-    char * as_writable_buffer(T & value) {
-        // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-        return reinterpret_cast<char *>(&value);
-    }
 };
 
 class Block{
