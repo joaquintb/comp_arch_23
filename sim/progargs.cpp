c22235478e87b61fa3d8d2e7896999abc30f4859
joaquintb <138466740+joaquintb@users.noreply.github.com>
2023-11-18 11:18:18 +0100

    Update progargs.cpp

--- sim/progargs.cpp
+++ sim/progargs.cpp
@@ -1,37 +1,38 @@
 #include "progargs.hpp"
 
-void inputTest(int argc, char ** argv) {
-  // handling alternative to direct use of char **argv
-  std::span const args_view{argv, static_cast<std::size_t>(argc)};
-  std::vector<std::string> const arguments{args_view.begin() + 1, args_view.end()};
-
-  std::ifstream input(arguments[1]);
-  std::ifstream output(arguments[2]);
-
-  bool numeric = true;
-
-  for (int i = 0; i < arguments[0].size(); i++) {
-    if (isdigit(arguments[0][i]) == false && arguments[0][i] != '-') { numeric = false; }
-  }
-
+int handle_num_args(int argc, std::string const & steps) {
+  // (-1): invalid number of arguments
   if (argc - 1 != 3) {
-    std::cerr << "Error: Invalid number of arguments: " << std::to_string(argc - 1) << std::endl;
+    std::cerr << "Error: Invalid number of arguments: " << std::to_string(argc - 1) << "\n";
     std::exit(-1);
   }
-  if (numeric == false) {
-    std::cerr << "Error: time steps must be numeric." << std::endl;
+  // (-1): first argument integer number
+  int n_steps = 0;
+  try {
+    n_steps = std::stoi(steps);
+  } catch (std::invalid_argument const & e) {
+    std::cerr << "Error: time steps must be numeric."
+              << "\n";
     std::exit(-1);
   }
-  if (std::stoi(arguments[0]) < 0) {
-    std::cerr << "Error: Invalid number of time steps." << std::endl;
+  // (-2): invalid number of time steps
+  if (n_steps < 0) {
+    std::cerr << "Error: Invalid number of time steps."
+              << "\n";
     std::exit(-2);
   }
-  if (!input.is_open()) {
-    std::cerr << "Error: Cannot open " << arguments[1] << " for reading" << std::endl;
+  return n_steps;
+}
+
+void check_files(std::ifstream & inputFile, std::string const & inputName,
+                 std::ofstream & outputFile, std::string const & outputName) {
+  if (!inputFile.is_open()) {
+    std::cerr << "Error: Cannot open " << inputName << " for reading.\n";
     std::exit(-3);
   }
-  if (!output.is_open()) {
-    std::cerr << "Error: Cannot open " << arguments[2] << " for writing" << std::endl;
+
+  if (!outputFile.is_open()) {
+    std::cerr << "Error: Cannot open " << outputName << " for writing.\n";
     std::exit(-4);
   }
 }
