d865c0e65986d703e756f68a7a7bad4b8cd38f1b
jyanespu <132780169+jyanespu@users.noreply.github.com>
2023-11-22 18:27:44 +0100

    Update run_test.sh
    Run unit tests first

--- run_test.sh
+++ run_test.sh
@@ -3,7 +3,7 @@
 . /etc/profile
 module avail
 module load gcc/12.1.0
-echo "*** Running Functional Tests ***"
-./build/ftest/ftest
 echo "*** Running Unit Tests ***"
 ./build/utest/utest
+echo "*** Running Functional Tests ***"
+./build/ftest/ftest
